60ec5f4f5f3848f4c83193b1d1ee719882493846
Zilian <13718722639leo@gmail.com>
2022-07-12 18:56:45 +0200

    add convex hull IOU and update kalman filter

--- src/sort_instance.py
+++ src/sort_instance.py
@@ -4,17 +4,18 @@
 from __future__ import print_function
 
 import os
+from typing import List, Tuple
 import numpy as np
-import matplotlib
-#matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from skimage import io
 
-import glob
 import time
 import argparse
 from filterpy.kalman import KalmanFilter
+from sklearn import cluster
+
+from distance import convex_hull_iou, euclidea_distance
 
 np.random.seed(0)
 
@@ -30,24 +31,18 @@ def linear_assignment(cost_matrix):
     return np.array(list(zip(x, y)))
 
 
-def iou_batch(bb_test, bb_gt):
+def iou_batch(segment_instances, tracked_instances):
   """
   From SORT: Computes IOU between two instances in the form [x1,y1,x2,y2]
   return IOU matrix, the negative of cost matrix
   """
-  bb_gt = np.expand_dims(bb_gt, 0)
-  bb_test = np.expand_dims(bb_test, 1)
-  
-  xx1 = np.maximum(bb_test[..., 0], bb_gt[..., 0])
-  yy1 = np.maximum(bb_test[..., 1], bb_gt[..., 1])
-  xx2 = np.minimum(bb_test[..., 2], bb_gt[..., 2])
-  yy2 = np.minimum(bb_test[..., 3], bb_gt[..., 3])
-  w = np.maximum(0., xx2 - xx1)
-  h = np.maximum(0., yy2 - yy1)
-  wh = w * h
-  o = wh / ((bb_test[..., 2] - bb_test[..., 0]) * (bb_test[..., 3] - bb_test[..., 1])                                      
-    + (bb_gt[..., 2] - bb_gt[..., 0]) * (bb_gt[..., 3] - bb_gt[..., 1]) - wh)                                              
-  return(o)  
+  num_seg = len(segment_instances) 
+  num_track = len(tracked_instances)
+  iou_matrix = np.zeros((num_seg, num_track))
+  for row in range(num_seg):
+    for col in range(num_track):
+      iou_matrix[row, col] = convex_hull_iou(segment_instances[row], tracked_instances[col])
+  return iou_matrix
 
 
 # def convert_bbox_to_z(bbox):
@@ -77,28 +72,61 @@ def iou_batch(bb_test, bb_gt):
 #   else:
 #     return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.,score]).reshape((1,5))
 
+def convert_centroid_to_instance(state:np.ndarray, frame:dict)->np.ndarray:
+  '''
+  Given updated state of instance centeroid, find the closest segemented instance
+  '''
+  pred_x, pred_y = state[:2]
+  min_distance = 1e5
+  optimal_instance = None
+  for instance in frame.values():
+    cluster = instance['points']
+    segement_centroid_x, segement_centroid_y = extract_instance_centeroid(cluster)
+    distance = euclidea_distance(segement_centroid_x, segement_centroid_y, 
+                                  pred_x, pred_y)
+    if min_distance > distance:
+      min_distance = distance
+      optimal_instance = instance # points with class ID
+  return optimal_instance
+
+
+def extract_instance_centeroid(instance:np.ndarray)->Tuple[float, float]:
+  '''
+  extract the centeroid of an instance by getting the mean of x, y
+  '''
+  x_center = instance[:, 0].mean()
+  y_center = instance[:, 1].mean()
+  return (x_center, y_center)
+
 
 class KalmanBoxTracker(object):
   """
   This class represents the internal state of individual tracked objects observed as bbox.
   """
   count = 0
-  def __init__(self,bbox):
+  def __init__(self,cluster):
     """
     Initialises a tracker using initial bounding box.
     """
     #define constant velocity model
-    self.kf = KalmanFilter(dim_x=7, dim_z=4) 
-    self.kf.F = np.array([[1,0,0,0,1,0,0],[0,1,0,0,0,1,0],[0,0,1,0,0,0,1],[0,0,0,1,0,0,0],  [0,0,0,0,1,0,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,1]])
-    self.kf.H = np.array([[1,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,1,0,0,0,0],[0,0,0,1,0,0,0]])
+    self.kf = KalmanFilter(dim_x=4, dim_z=2) 
+    self.kf.F = np.array([[1,0,1,0],[0,1,0,1],[0,0,1,0],[0,0,0,1]])
+    self.kf.H = np.array([[1,0,0,0],[0,1,0,0]])
+    # F is the transition matrix with constant velocity motion model, which will be used to multiply with vector [x, y, x_v, y_v]^T in prediction step.
+    # H is measurement matrix which represents the measurement model in update step.
+    # Motion noise covariance matrix, Q
+    # Measurement noise covariance matrix, R
+    # State covariance matrix, P
 
-    self.kf.R[2:,2:] *= 10.
+  # self.kf.R[2:,2:] *= 10.  #?  corresponds to s, r
     self.kf.P[4:,4:] *= 1000. #give high uncertainty to the unobservable initial velocities
-    self.kf.P *= 10.
-    self.kf.Q[-1,-1] *= 0.01
-    self.kf.Q[4:,4:] *= 0.01
+    # in this way, we can choose if we trust measurement!!!!
+    self.kf.P *= 10.   # initial value??
+    # self.kf.Q[-1,-1] *= 0.01
+    # self.kf.Q[4:,4:] *= 0.01
 
-    self.kf.x[:4] = convert_bbox_to_z(bbox)
+
+    self.kf.x[:2] = extract_instance_centeroid(cluster)
     self.time_since_update = 0
     self.id = KalmanBoxTracker.count
     KalmanBoxTracker.count += 1
@@ -107,7 +135,7 @@ class KalmanBoxTracker(object):
     self.hit_streak = 0
     self.age = 0
 
-  def update(self,bbox):
+  def update(self,cluster):
     """
     Updates the state vector with observed bbox.
     """
@@ -115,7 +143,7 @@ class KalmanBoxTracker(object):
     self.history = []
     self.hits += 1
     self.hit_streak += 1
-    self.kf.update(convert_bbox_to_z(bbox))
+    self.kf.update(extract_instance_centeroid(cluster))
 
   def predict(self):
     """
@@ -128,26 +156,27 @@ class KalmanBoxTracker(object):
     if(self.time_since_update>0):
       self.hit_streak = 0
     self.time_since_update += 1
-    self.history.append(convert_x_to_bbox(self.kf.x))
+    self.history.append(convert_centroid_to_instance(self.kf.x))
     return self.history[-1]
 
   def get_state(self):
     """
     Returns the current bounding box estimate.
     """
-    return convert_x_to_bbox(self.kf.x)
+    return convert_centroid_to_instance(self.kf.x)
 
 
-def associate_detections_to_trackers(detections,trackers,iou_threshold = 0.3):
+def associate_detections_to_trackers(instances, trackers, iou_threshold = 0.3):
   """
-  Assigns detections to tracked object (both represented as bounding boxes)
+  Assigns instance segmentations to tracked object (both represented as clusters)
+  param trackers: trackers' predictions in this frame
 
   Returns 3 lists of matches, unmatched_detections and unmatched_trackers
   """
   if(len(trackers)==0):
-    return np.empty((0,2),dtype=int), np.arange(len(detections)), np.empty((0,5),dtype=int)
+    return np.empty((0,2),dtype=int), np.arange(len(instances)), np.empty((0,5),dtype=int)  # change!  # 为什么中间那个是arange，其他两个是empty？
 
-  iou_matrix = iou_batch(detections, trackers)
+  iou_matrix = iou_batch(instances, trackers)
 
   if min(iou_matrix.shape) > 0:
     a = (iou_matrix > iou_threshold).astype(np.int32)
@@ -194,29 +223,30 @@ class Sort(object):
     self.trackers = []
     self.frame_count = 0
 
-  def update(self, dets=np.empty((0, 5))):
+  def update(self, clusters:List[np.ndarray]):
     """
     Params:
-      dets - a numpy array of detections in the format [[x1,y1,x2,y2,score],[x1,y1,x2,y2,score],...]
+      clusters - a list of ndarray 
     Requires: this method must be called once for each frame even with empty detections (use np.empty((0, 5)) for frames without detections).
     Returns the a similar array, where the last column is the object ID.
 
     NOTE: The number of objects returned may differ from the number of detections provided.
     """
     self.frame_count += 1
-    # get predicted locations from existing trackers.
-    trks = np.zeros((len(self.trackers), 5))
+    # get predicted locations from existing trackers, associate the detections with predictions
+    #trks = np.zeros((len(self.trackers), 5))  
+    trks = []  
     to_del = []
     ret = []
-    for t, trk in enumerate(trks):
-      pos = self.trackers[t].predict()[0]
-      trk[:] = [pos[0], pos[1], pos[2], pos[3], 0]
-      if np.any(np.isnan(pos)):
-        to_del.append(t)
-    trks = np.ma.compress_rows(np.ma.masked_invalid(trks))
-    for t in reversed(to_del):
-      self.trackers.pop(t)
-    matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(dets,trks, self.iou_threshold)
+    for t in range(len(self.trackers)):
+      pred = self.trackers[t].predict()[0]
+      trks.append(pred)
+    #   if np.any(np.isnan(pos)):   # if any of the predictions is Nan, delete the tracker #？？？？ 
+    #     to_del.append(t)
+    # trks = np.ma.compress_rows(np.ma.masked_invalid(trks))
+    # for t in reversed(to_del):
+    #   self.trackers.pop(t)
+    matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(clusters,trks, self.iou_threshold)
 
     # update matched trackers with assigned detections
     for m in matched:
@@ -224,7 +254,7 @@ class Sort(object):
 
     # create and initialise new trackers for unmatched detections
     for i in unmatched_dets:
-        trk = KalmanBoxTracker(dets[i,:])
+        trk = KalmanBoxTracker(dets[i,:])      
         self.trackers.append(trk)
     i = len(self.trackers)
     for trk in reversed(self.trackers):
@@ -281,29 +311,29 @@ if __name__ == '__main__':
                       iou_threshold=args.iou_threshold) #create instance of the SORT tracker
   
   for frame_idx, frame in enumerate(sequence_segments.item()):  
-    dets[:, 2:4] += dets[:, 0:2] #convert to [x1,y1,w,h] to [x1,y1,x2,y2] #????? 不需要转换 
+    clusters = [instance['points'] for instance in frame.values()]
 
-    if(display):
-      fn = os.path.join('mot_benchmark', phase, seq, 'img1', '%06d.jpg'%(frame))
-      im =io.imread(fn)
-      ax1.imshow(im)
-      plt.title(seq + ' Tracked Targets')
+    # if(display):
+    #   fn = os.path.join('mot_benchmark', phase, seq, 'img1', '%06d.jpg'%(frame))
+    #   im =io.imread(fn)
+    #   ax1.imshow(im)
+    #   plt.title(seq + ' Tracked Targets')
 
     start_time = time.time()
-    trackers = mot_tracker.update(dets)
+    trackers = mot_tracker.update(clusters)
     cycle_time = time.time() - start_time
     total_time += cycle_time
 
-    for d in trackers:
-      print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1'%(frame,d[4],d[0],d[1],d[2]-d[0],d[3]-d[1]),file=out_file)
-      if(display):
-        d = d.astype(np.int32)
-        ax1.add_patch(patches.Rectangle((d[0],d[1]),d[2]-d[0],d[3]-d[1],fill=False,lw=3,ec=colours[d[4]%32,:]))
+    # for d in trackers:
+    #   print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1'%(frame,d[4],d[0],d[1],d[2]-d[0],d[3]-d[1]),file=out_file)
+    #   if(display):
+    #     d = d.astype(np.int32)
+    #     ax1.add_patch(patches.Rectangle((d[0],d[1]),d[2]-d[0],d[3]-d[1],fill=False,lw=3,ec=colours[d[4]%32,:]))
 
-    if(display):
-      fig.canvas.flush_events()
-      plt.draw()
-      ax1.cla()
+    # if(display):
+    #   fig.canvas.flush_events()
+    #   plt.draw()
+    #   ax1.cla()
 
   print("Total Tracking took: %.3f seconds for %d frames or %.1f FPS" % (total_time, frame+1, (frame+1) / total_time))
 
